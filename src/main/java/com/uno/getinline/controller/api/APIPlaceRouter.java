9145228e3252def370523b8e1b25708c64044e8d
youngin <uyoungin@naver.com>
2025-02-10 09:43:33 +0900

    Api Method test
    merge 하지는 않음

---
+++ src/main/java/com/uno/getinline/controller/api/APIPlaceRouter.java
@@ -0,0 +1,33 @@
+package com.uno.getinline.controller.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import javax.xml.xpath.XPath;
+import java.util.List;
+
+import static org.springframework.web.servlet.function.RequestPredicates.path;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
+@Configuration
+public class APIPlaceRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> placeRouter(APIPlaceHandler apiPlaceHandler){
+        return route().nest(path("/api/places"), builder -> builder
+                .GET("", apiPlaceHandler::getPlaces)
+                .POST("", apiPlaceHandler::createPlace)
+                .GET("/{placeId}", apiPlaceHandler::getPlace)
+                .PUT("/{placeId}", apiPlaceHandler::modifyPlaces)
+                .DELETE("/{placeId}", apiPlaceHandler::removePlaces)
+                ).build();
+    }
+    
+    //placeRouter 메서드가 아래 와 같은 내용임
+    /*@GetMapping("/api/places")
+    public List<String> getPlaces(){return List.of("place1", "place2");}*/
+
+}
